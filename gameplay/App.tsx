1499a50ea506756a65dec5fda5349f4ea9a46ee7
StormKing22 <144955631+clarardgs@users.noreply.github.com>
2024-04-26 11:57:58 -0300

    react
    nada com nada

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text} from 'react-native'
 
-export  default function app (){
+export default function Signin(){
+   return (
+  <View>
 
-return (
-   <View>
-      <Text>Clara</Text>
-   </View> 
- );
- 
-}
+   <Text>Clarinha</Text>
+   </View>
+
+   );
+
