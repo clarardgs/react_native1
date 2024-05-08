227b0fbff5fc849b73f32f0332095d4739c930cd
StormKing22 <144955631+clarardgs@users.noreply.github.com>
2024-05-08 11:56:48 -0300

    react_native
    Oi

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View,Text} from 'react-native'
 
-export default function Signin(){
+export default function app(){
    return (
   <View>
 
@@ -9,4 +9,4 @@ export default function Signin(){
    </View>
 
    );
-
+}
