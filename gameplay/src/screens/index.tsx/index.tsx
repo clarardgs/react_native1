d380ce5fec248764098146278439ddbaa9ff3380
joaopedrordm77 <>
2024-05-15 11:55:50 -0300

    react
    nada!!

--- gameplay/src/screens/index.tsx/index.tsx
+++ gameplay/src/screens/index.tsx/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View,Text} from 'react-native'
+import {View,Text, TextInput} from 'react-native'
 import {styles} from './styles'
 export  function SignIn() {
+  const [text, setText] = useState ("Nada digitado ainda");
+
    return (
-    <View style={{flex:1,blackgroundcolor:'black',alignItems:'center'}
-      <Text>clarinha</Text>
-    </View>
-}
-    
-);
-}
+    <View style={styles.container}>
+      <Text>   
+        clarinha
+
+      </Text>
+      <TextInput style={styles.input}/>
+
+         <Text>
+           
+           Digitou: {text}
+         </Text>
+      import {View , Text, TextInout } from 'react-native';
+    </View>  
+)}
--- gameplay/src/screens/index.tsx/styles.ts
+++ gameplay/src/screens/index.tsx/styles.ts
@@ -1,9 +1,14 @@
 import {StyleSheet} from "react-native";
 
-export const styles = StyLeSheet.create({
+export const styles = StyleSheet.create({
         container:{
             flex:1,
-            backgroundColor:"black",
-            justify
+            backgroundColor:'black',
+            justifyContent:'center',
         }
-    })  
+        input: {
+            heigth: 50,
+            width: 200,
+            borderBottomWidth: 2,
+        }
+    });
