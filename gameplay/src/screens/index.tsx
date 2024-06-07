f89cfc6126a8c363cc575acf9e834633729c9289
joaopedrordm77 <144955631+clarardgs@users.noreply.github.com>
2024-06-07 11:55:11 -0300

    não terminado
    é a vida né?

--- gameplay/src/screens/index.tsx
+++ gameplay/src/screens/index.tsx
@@ -1,16 +1,19 @@
 import React, {useState} from "react";
-import {View,Text,TextInput,Image} from 'react-native'
+import {StyleSheet,View,Text,TextInput,Image,StatuBar} from 'react-native';
 import {styles} from  './styles'
 import illustrationImg from '../../assets/illustrationImg.png';
+import { StatusBar } from "expo-status-bar";
+
 export function  signIn() {
 
     const [text,setText] = useState ("nada digitado ainda")
 
+  
     return (
 
         <View style={styles.container}>
-          <Image source={illustrationImg} style={styles.image}resizeMode='stretch'/>
-          <View style={styles.content}>
+           <Image source={illustrationImg} style={styles.image}resizeMode='stretch'/>
+           <View style={styles.content}>
              <Text style={styles.title}>
                
                Organize suas {`\n`}
